41818c8b8a4d9283cf1cece65914fa5e1995a232
Saint <ss.mscx@gmail.com>
2026-07-25 19:11:55 -0600

    conclusión de convertor agregando la funcionalidad para hasta y que pueda verse reflejado en el boton

--- convertir.php
+++ convertir.php
@@ -30,13 +30,42 @@ function convertir_a_metros($valor, $unidad_desde){
     }
 }
 
+function convertir_desde_metros($valor, $unidad_hasta){
+    switch($unidad_hasta){
+
+        case 'Milimetro':
+            return $valor * 1000;
+        case 'Centimetro':
+            return $valor * 100;
+        case 'Decimetro':
+            return $valor * 10;
+        case 'Metro':
+            return $valor;
+        case 'Decametro':
+            return $valor / 10;
+        case 'Hectometro':
+            return $valor / 100;
+        case 'Kilometro':
+            return $valor / 1000;
+        break;
+        default:
+            return 'Unidad no válida';
+        break;
+    }
+}
+
+
+
+
 if(isset($_POST['convertir'])){
     $valor = $_POST['valor'];
     $desde = $_POST['desde'];
+    $hasta = $_POST['hasta'];
     
     $calculoDesde = convertir_a_metros($valor, $desde);
-
-    $resultado = $calculoDesde;
+    $calcularHasta = convertir_desde_metros($calculoDesde, $hasta);
+;
+    $resultado = number_format($calcularHasta, 2);
 }
 
 
